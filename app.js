0e665bb0f292c64626996923beeafcf6a3bd9c09
Preethi-Bojarajan <58773715+Preethi-Bojarajan@users.noreply.github.com>
2021-01-07 09:56:05 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 6070
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:priti7pg@localhost:5432/postgres',
+    ssl: process.env.DATABASE_URL ? true : false
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
